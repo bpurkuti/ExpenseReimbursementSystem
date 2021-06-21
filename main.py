a12c776937c50ee2e1c860be5fc3c3c5ebd8006d
Bishwo Purkuti <bishwopurkutihhs@gmail.com>
2021-06-20 21:04:10 -0700

    final

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 
 from flask import Flask
 from flask_cors import CORS
@@ -5,6 +6,8 @@ from flask_cors import CORS
 from routes import employee_routes, reimbursement_routes
 
 app: Flask = Flask(__name__)
+logging.basicConfig(filename="records.log", level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(message)s')
+
 CORS(app)
 employee_routes.create_routes(app)
 reimbursement_routes.create_routes(app)
